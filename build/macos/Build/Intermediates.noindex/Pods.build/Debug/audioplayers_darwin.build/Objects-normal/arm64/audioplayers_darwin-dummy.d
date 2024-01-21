b79d7c1d59a3aa99468d536fd1420223de5532ee
Tymoteusz Buczkowski <tymoteusz.buczkowski@droidsonroids.pl>
2024-01-21 14:51:22 +0100

    Rerun gitignore

--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/audioplayers_darwin.build/Objects-normal/arm64/audioplayers_darwin-dummy.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/tymoteuszbuczkowski/Workspace/hgj2024/build/macos/Build/Intermediates.noindex/Pods.build/Debug/audioplayers_darwin.build/module.modulemap \
-  /Users/tymoteuszbuczkowski/Workspace/hgj2024/macos/Pods/Target\ Support\ Files/audioplayers_darwin/audioplayers_darwin-dummy.m \
-  /Users/tymoteuszbuczkowski/Workspace/hgj2024/macos/Pods/Target\ Support\ Files/audioplayers_darwin/audioplayers_darwin-prefix.pch
--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/shared_preferences_foundation.build/Objects-normal/arm64/shared_preferences_foundation-dummy.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/tymoteuszbuczkowski/Workspace/hgj2024/build/macos/Build/Intermediates.noindex/Pods.build/Debug/shared_preferences_foundation.build/module.modulemap \
-  /Users/tymoteuszbuczkowski/Workspace/hgj2024/macos/Pods/Target\ Support\ Files/shared_preferences_foundation/shared_preferences_foundation-dummy.m \
-  /Users/tymoteuszbuczkowski/Workspace/hgj2024/macos/Pods/Target\ Support\ Files/shared_preferences_foundation/shared_preferences_foundation-prefix.pch
