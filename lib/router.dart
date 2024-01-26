7f4462364b1fc12a66405ced1f2bcaa92deaa83a
Tymoteusz Buczkowski <tymoteusz.buczkowski@droidsonroids.pl>
2024-01-26 19:56:32 +0100

    Remove useless shit

--- lib/router.dart
+++ lib/router.dart
@@ -8,16 +8,8 @@ import 'package:hackerspace_game_jam_2024/3d_renderer/3d_renderer_page.dart';
 import 'package:hackerspace_game_jam_2024/game/game_page.dart';
 import 'package:hackerspace_game_jam_2024/main_menu/main_menu_screen.dart';
 import 'package:hackerspace_game_jam_2024/raymarching/raymarching2d_page.dart';
-import 'package:provider/provider.dart';
 
-import 'game_internals/score.dart';
-import 'level_selection/level_selection_screen.dart';
-import 'level_selection/levels.dart';
-import 'play_session/play_session_screen.dart';
 import 'settings/settings_screen.dart';
-import 'style/my_transition.dart';
-import 'style/palette.dart';
-import 'win_game/win_game_screen.dart';
 
 /// The router describes the game's navigational hierarchy, from the main
 /// screen through settings screens all the way to each individual level.
@@ -28,57 +20,9 @@ final router = GoRouter(
       builder: (context, state) => const MainMenuScreen(key: Key('main menu')),
       routes: [
         GoRoute(
-            path: 'play',
-            pageBuilder: (context, state) => buildMyTransition<void>(
-                  key: ValueKey('play'),
-                  color: context.watch<Palette>().backgroundLevelSelection,
-                  child: const LevelSelectionScreen(
-                    key: Key('level selection'),
-                  ),
-                ),
-            routes: [
-              GoRoute(
-                path: 'session/:level',
-                pageBuilder: (context, state) {
-                  final levelNumber = int.parse(state.pathParameters['level']!);
-                  final level = gameLevels.singleWhere((e) => e.number == levelNumber);
-                  return buildMyTransition<void>(
-                    key: ValueKey('level'),
-                    color: context.watch<Palette>().backgroundPlaySession,
-                    child: PlaySessionScreen(
-                      level,
-                      key: const Key('play session'),
-                    ),
-                  );
-                },
-              ),
-              GoRoute(
-                path: 'won',
-                redirect: (context, state) {
-                  if (state.extra == null) {
-                    // Trying to navigate to a win screen without any data.
-                    // Possibly by using the browser's back button.
-                    return '/';
-                  }
-
-                  // Otherwise, do not redirect.
-                  return null;
-                },
-                pageBuilder: (context, state) {
-                  final map = state.extra! as Map<String, dynamic>;
-                  final score = map['score'] as Score;
-
-                  return buildMyTransition<void>(
-                    key: ValueKey('won'),
-                    color: context.watch<Palette>().backgroundPlaySession,
-                    child: WinGameScreen(
-                      score: score,
-                      key: const Key('win game'),
-                    ),
-                  );
-                },
-              )
-            ]),
+          path: 'game_page',
+          builder: (context, state) => const GamePage(key: Key('game_page')),
+        ),
         GoRoute(
           path: 'settings',
           builder: (context, state) => const SettingsScreen(key: Key('settings')),
@@ -90,10 +34,6 @@ final router = GoRouter(
         GoRoute(
           path: 'raymarching',
           builder: (context, state) => const Raymarching2DPage(key: Key('3d_renderer')),
-        ),
-        GoRoute(
-          path: 'game_page',
-          builder: (context, state) => const GamePage(key: Key('game_page')),
         ),
       ],
     ),
