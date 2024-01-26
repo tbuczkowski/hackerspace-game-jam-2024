a3b4b02f8dff0f54b8ec77138806e3b2139df9e8
palucdev <palucdev@gmail.com>
2024-01-26 20:04:12 +0100

    Assets

---
+++ lib/game/asd.dart
@@ -0,0 +1,21 @@
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+
+class ASDGame extends FlameGame {
+  ASDGame();
+
+  @override
+  Future<void> onLoad() async {
+    await images.loadAll([
+      'block.png',
+      'ember.png',
+      'ground.png',
+      'heart_half.png',
+      'heart.png',
+      'star.png',
+      'water_enemy.png',
+    ]);
+
+    camera.viewfinder.anchor = Anchor.topLeft;
+  }
+}
--- lib/game/game_page.dart
+++ lib/game/game_page.dart
@@ -1,6 +1,8 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
+import 'asd.dart';
+
 class GamePage extends StatefulWidget {
   const GamePage({super.key});
 
@@ -9,7 +11,7 @@ class GamePage extends StatefulWidget {
 }
 
 class _GamePageState extends State<GamePage> {
-  final FlameGame _game = FlameGame();
+  final FlameGame _game = ASDGame();
 
   @override
   Widget build(BuildContext context) {
